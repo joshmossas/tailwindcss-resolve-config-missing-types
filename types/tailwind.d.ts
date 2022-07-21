3536e6b96646d650b999288295c2a586804b15a0
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2022-07-21 13:01:55 -0500

    get ts-node working

---
+++ types/tailwind.d.ts
@@ -0,0 +1,5 @@
+// Current solution. Uncomment this to get the project working
+// declare module "tailwindcss/resolveConfig" {
+//   import type { Config } from "tailwindcss";
+//   export default function (config: Config): Config;
+// }
